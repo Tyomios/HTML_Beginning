fb3e93f63557b26abc24c44da6206f522f9ea77b
Tyomios <tyomios@mail.ru>
2022-12-02 11:58:24 +0700

    Добавлены обрыботки события наведения мыши на кнопки.

---
+++ scripts/divDocFuncs.js
@@ -0,0 +1,11 @@
+function setMouseOverBntView(obj)
+{
+    var btn = document.getElementById(obj);
+    btn.className = "news_input_mouse_over";
+}
+
+function setNewsBtnView(obj)
+{
+    var btn = document.getElementById(obj);
+    btn.className = "news_input";
+}
